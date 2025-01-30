3e8d7416f9157f2641f2d8fdc5c56d9bb6a10261
Theerom <dectrom50@gmail.com>
2025-01-30 11:14:39 +0300

    fix20

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,11 +20,7 @@ export default function RootLayout({
       <Head>
         <title>Earn Credits App</title>
         <meta property="og:title" content="Earn-paper App" />
-        <meta property="og:description" content="Earn Credits, Unlock Rewards!" />
-        <meta property="og:image" content="https://images2.imgbox.com/3b/be/tc9OTXTO_o.jpg" />
-        <meta property="og:url" content="https://earn-paper.vercel.app" />
-        <meta property="og:type" content="website" />
-        <meta property="og:site_name" content="Earn-paper App" />
+        
       </Head>
       <body className={inter.className}>
         <div id="app-root">{children}</div>
