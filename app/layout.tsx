a26763553d4888357d2b37006591cd0d303ce380
Theerom <dectrom50@gmail.com>
2025-02-07 22:13:27 +0300

    fix27

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import type { Metadata } from "next"
 import { Inter } from 'next/font/google'
 import "./globals.css"
 import Head from 'next/head';
@@ -8,11 +7,6 @@ import { useEffect } from 'react';
 import { updateReferrerCredits } from '../utils/updateReferrerCredits';
 
 const inter = Inter({ subsets: ["latin"] })
-
-export const metadata: Metadata = {
-  title: "Earn-paper App",
-  description: "Earn Credits, Unlock Rewards!",
-}
 
 export default function RootLayout({
   children,
---
+++ app/metadata.ts
@@ -0,0 +1,6 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Earn-paper App",
+  description: "Earn Credits, Unlock Rewards!",
+}; 
